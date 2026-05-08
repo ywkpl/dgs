09adcf1d4ce46c7c6ee66f880592d9b5cf4b621a
ywk <ywkpl@hotmail.com>
2026-05-08 18:16:47 +0800

    共用新增/更新EntityView,並繼承自查詢

--- src/main/java/com/dotest/dsg/entityview/BankInfoUpdateView.java
+++ src/main/java/com/dotest/dsg/entityview/BankInfoUpdateView.java
@@ -6,32 +6,18 @@ import com.dotest.dsg.entity.BankInfo;
 @EntityView(BankInfo.class)
 @CreatableEntityView
 @UpdatableEntityView
-public interface BankInfoUpdateView {
-    @IdMapping
-    Long getId();
+public interface BankInfoUpdateView extends BankInfoView {
     void setId(Long id);
 
-    Boolean getEnable();
     void setEnable(Boolean enable);
 
-    String getTel();
     void setTel(String tel);
 
-    String getAddr();
     void setAddr(String addr);
 
-    String getName();
     void setName(String name);
 
-    String getBranchCode();
     void setBranchCode(String branchCode);
 
-    String getCode();
     void setCode(String code);
-
-    @Mapping("CONCAT(" +
-            "COALESCE(CONCAT(code, '-'), ''), " +
-            "COALESCE(CONCAT(branchCode, ' '), ''), " +
-            "COALESCE(name, ''))")
-    String getBankInfoName();
 }
